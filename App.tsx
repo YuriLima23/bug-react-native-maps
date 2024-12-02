49e20022ce39ea30d652950c56ca1a78871c06d9
Yuri Ramos <yuri.lima@garupa.co>
2024-12-02 19:26:34 -0300

    feat: create simple repo to reproduce bug using react-native maps

--- App.tsx
+++ App.tsx
@@ -1,13 +1,47 @@
+import { createStaticNavigation, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+
+
+const Map = () => {
+  const position = {latitude: 40.68816862892598, longitude:  -73.99758773340365}
+  return (
+    <MapView initialRegion={{...position, latitudeDelta: 0.009, longitudeDelta: 0.009}} style={StyleSheet.absoluteFillObject}>
+      <Marker coordinate={position} title='Local'>
+
+      </Marker>
+
+    </MapView>
+  )
+}
+
+const Home = () => {
+  const navigation = useNavigation()
+  return (
+    <View style={styles.regionButton}>
+      <Button title='Go to Maps' onPress={() => navigation.navigate("Map")} />
+
+    </View>)
+}
+
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Map',
+  screens: {
+    Home: Home,
+    Map: Map,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Navigation />
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +51,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  regionButton:{
+    flex: 1, 
+    backgroundColor: "#fff",
+    justifyContent:"center" 
+  }
 });
